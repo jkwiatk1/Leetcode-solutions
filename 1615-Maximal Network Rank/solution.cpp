1227f45d7f1db0cb897409929c21eab43b4caac3
jkwiatk1 <01142696@pw.edu.pl>
2023-08-18 11:46:24 +0200

    Init exercise

---
+++ 1615-Maximal Network Rank/solution.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <vector> 
+
+using namespace std; 
+
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        int max_net_rank = 0;
+
+        return max_net_rank;       
+    }
+};
+
+int main(){
+    Solution solution;
+    return 0;
+}
