7e184412f303d3796fa37cc922ea511d1aa9d3d1
jkwiatk1 <01142696@pw.edu.pl>
2023-08-22 09:46:13 +0200

    Add second solution

--- 242-Valid Anagram/solution.cpp
+++ 242-Valid Anagram/solution.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
+    //Solution 1
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
         if(s == t) return true;
         else return false;
+    }
+
+    //Solution 2
+    bool isAnagram2(string s, string t) {
+        std::unordered_map<char, int> charCount;
+
+        for (char c : s) {
+            charCount[c]++;
+        }
+
+        for (char c : t) {
+            charCount[c]--;
+        }
+
+        for (const auto& pair : charCount) {
+            if (pair.second != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
 
